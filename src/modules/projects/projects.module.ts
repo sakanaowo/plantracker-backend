623a59349a5a5b293381919f815d9ac2f4e82a5d
sakanaowo <idiotbugg@gmail.com>
2025-10-29 10:52:58 +0700

    feat: Add ActivityLogsController with endpoints for workspace, project, task, and user activity feeds; update module exports

--- src/modules/projects/projects.module.ts
+++ src/modules/projects/projects.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
 import { ActivityLogsModule } from '../activity-logs/activity-logs.module';
 
 @Module({
-  imports: [ActivityLogsModule],
+  imports: [forwardRef(() => ActivityLogsModule)],
   controllers: [ProjectsController],
   providers: [ProjectsService],
   exports: [ProjectsService],
